9882a53f099276599bb0888c5f2079db3dce3fe0
ClementNdome <clementndome20@gmail.com>
2026-02-25 22:42:56 +0300

    admin almost ready

--- obstacle_compliance/admin.py
+++ obstacle_compliance/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+# from django.contrib.gis.db import admin
+from leaflet.admin import LeafletGeoAdmin
+
+
 # Register your models here.
+from .models import Aerodrome #GazettedEstate
+
+
+class Aerodromes(LeafletGeoAdmin):
+    list_display = (
+        "name",
+        "icao_code",
+        "fid",
+        "type",
+        "admin_company",
+    )
+
+
+# admin.site.register(Aerodromes)
+admin.site.register(Aerodrome, Aerodromes)
+# admin.site.register(GazettedEstate)
