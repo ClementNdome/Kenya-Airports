652229a19985834ef7d01bf755a9418dab141ffb
ClementNdome <clementndome20@gmail.com>
2026-02-26 19:43:19 +0300

    'api/airport-points.geojson

--- obstacle_compliance/urls.py
+++ obstacle_compliance/urls.py
@@ -33,6 +33,8 @@ urlpatterns = [
     # Statistics
     path('api/stats/', views.StatisticsView.as_view(), name='api_stats'),
 
+    path('api/airport-points.geojson', views.AerodromePointsGeoJSONView.as_view(), name='airport_points_geojson'),
+
         # In obstacle_compliance/urls.py - add temporarily
     path('debug/', views.debug_geojson, name='debug'),
 ]
