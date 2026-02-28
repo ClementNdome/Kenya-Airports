f91d4bf47a8846506a76f00ff192e6e10cedefe0
ClementNdome <clementndome20@gmail.com>
2026-02-28 04:32:45 +0300

    orchestraitng ulrs to the dashboard

--- airports_kenya/urls.py
+++ airports_kenya/urls.py
@@ -23,7 +23,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('airports_strips.urls')),
+    # path('', include('airports_strips.urls')),
+    path('', include('obstacle_compliance.urls')),
+
     path('obstacle-compliance/', include('obstacle_compliance.urls')),
 ]
 
