f9e6303d8f15c5de200bf8a04fd73dc04bb08c79
ClementNdome <clementndome20@gmail.com>
2026-07-04 13:09:00 +0300

    fix: Debug

--- airports_kenya/settings.py
+++ airports_kenya/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = config("SECRET_KEY", default="dummy-build-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = config('DEBUG', default=False, cast=bool)  # <-- Read from .env
-DEBUG = True
+DEBUG = config("DEBUG", default=False)
 # ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="*").split(",")  # <-- Read from .env
 ALLOWED_HOSTS = ["*"]
 
