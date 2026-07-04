22662b994d84a07604de14777251e449d8760acf
ClementNdome <clementndome20@gmail.com>
2026-07-04 12:41:57 +0300

    feat: base url is compliace application

--- airports_kenya/urls.py
+++ airports_kenya/urls.py
@@ -23,8 +23,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('airports_strips.urls')),
-    path('obstacle-compliance/', include('obstacle_compliance.urls')),
+    path('kenya-airports/', include('airports_strips.urls')),
+    path('', include('obstacle_compliance.urls')),
 ]
 
 
